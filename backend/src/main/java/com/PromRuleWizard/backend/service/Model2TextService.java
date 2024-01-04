fbb7fb06e8e65282dfe6307caf2493d99bbb9e7f
hadryyassine <hadryyassine@gmail.com>
2024-01-04 22:53:44 +0100

    M2M service

--- backend/src/main/java/com/PromRuleWizard/backend/service/Model2TextService.java
+++ backend/src/main/java/com/PromRuleWizard/backend/service/Model2TextService.java
@@ -9,21 +9,20 @@ import org.eclipse.epsilon.emc.emf.InMemoryEmfModel;
 import jakarta.annotation.PostConstruct;
 
 public class Model2TextService implements M2TService {
-	@Override
-	public String model2TextTransformer(InMemoryEmfModel targetModel) throws Exception {
-		// getting the EGL module : 
+    @Override
+    public String model2TextTransformer(InMemoryEmfModel targetModel) throws Exception {
+        // getting the EGL module :
         IEglModule module = (IEglModule) new EglTemplateFactoryModuleAdapter();
-        // Parse PrometheusRule.egl : 
-        module.parse(new File("/PromotheusRule.egl")); 
-        if(!module.getParseProblems().isEmpty())
-        {
+        // Parse PrometheusRule.egl :
+        module.parse(new File("/PromotheusRule.egl"));
+        if (!module.getParseProblems().isEmpty()) {
             throw new RuntimeException(module.getParseProblems().get(0).toString());
         }
         // Make the document visible to the EGX program
         module.getContext().getModelRepository().addModel(targetModel);
         // ... and execute
         return module.execute() + "";
-		
-	}
+
+    }
 
 }
