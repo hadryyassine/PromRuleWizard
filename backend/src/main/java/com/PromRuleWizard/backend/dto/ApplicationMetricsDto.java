a3df947f8ed0c31767fe9f78e90494c3ce8aa326
hadryyassine <hadryyassine@gmail.com>
2024-01-05 01:17:42 +0100

    Fixing the DTO

--- backend/src/main/java/com/PromRuleWizard/backend/dto/ApplicationMetricsDto.java
+++ backend/src/main/java/com/PromRuleWizard/backend/dto/ApplicationMetricsDto.java
@@ -1,35 +1,16 @@
 package com.PromRuleWizard.backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ApplicationMetricsDto {
 
-		private String sourceFlexmi;
-		private String targetProm;
+	private String appMetricSource;
 
-		public ApplicationMetricsDto(String sourceFlexmi, String targetProm)
-		{
-			this.sourceFlexmi = sourceFlexmi;
-			this.targetProm = targetProm;
-		}
-
-		public String getSourceFlexmi()
-		{
-			return sourceFlexmi;
-		}
-
-		public void setSourceFlexmi(String sourceFlexmi)
-		{
-			this.sourceFlexmi = sourceFlexmi;
-		}
-
-		public String getTargetProm()
-		{
-			return targetProm;
-		}
-
-		public void setTargetProm(String targetProm)
-		{
-			this.targetProm = targetProm;
-		}
 	}
 	
 	
